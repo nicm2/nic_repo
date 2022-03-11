054315b844747d4e8e5494a6521b8b86cce68fe6
nicm2 <89167131+nicm2@users.noreply.github.com>
2022-03-11 13:58:47 -0800

    Update menu.py

--- menu.py
+++ menu.py
@@ -4,36 +4,77 @@ def f1():
     print('f1')
 def f2():
     print('f2')
+# Main list of [Prompts, Actions]
+# Two styles are supported to execute abstracted logic
+# 1. file names will be run by exec(open("filename.py").read())
+# 2. function references will be executed directly file.function()
+main_menu = [
+    ["Christmas Tree", "christmastree.py"],
+    ["Ship", "ship.py"],
+]
 
-def buildMenu(menu):
-    for key,value in menu.items():
-        display = value["display"]
-        print(f"{key} ------ {display}")
-    print("What is your choice? (enter the number value) ")
+# Menu banner is typically defined by menu owner
+border = "=" * 25
+banner = f"\n{border}\nPlease Select An Option\n{border}"
 
-def presentMenu(menu):
-    buildMenu(menu)
-    choice = int(input())
-    if (choice) in menu:
-        if menu[choice]["type"] == "func":
-            menu[choice]["exec"]()
-        else:
-            presentMenu(menu[choice]["exec"])
-subMenu = {
-    1: {"display":"f1",
-    "exec":f1,
-    "type":"func"},
-    2: {"display":"f2",
-    "exec":f2,}
-}
-mainMenu = {
-    1: {"display":"Christmas Tree",
-    "exec":printTree,
-    "type":"func"},
-    2: {"display":"Ship",
-    "exec":printShip,
-    "type":"func"},
-}
+# def menu
+# using main_menu list:
+# 1. main menu and submenu reference are created [Prompts, Actions]
+# 2. menu_list is sent as parameter to menuy.menu function that has logic for menu control
+def menu():
+    title = "Function Menu" + banner
+    menu_list = main_menu.copy()
+    buildMenu(title, menu_list)
+
+# def submenu
+# using sub menu list above:
+# sub_menu works similarly to menu(
+
+def buildMenu(banner, options):
+    # header for menu
+    print(banner)
+    # build a dictionary from options
+    prompts = {0: ["Exit", None]}
+    for op in options:
+        index = len(prompts)
+        prompts[index] = op
+
+    # print menu or dictionary
+    for key, value in prompts.items():
+        print(key, '->', value[0])
+
+    # get user choice
+    choice = input("Type your choice> ")
+
+    # validate choice and run
+    # execute selection
+    # convert to number
+    try:
+        choice = int(choice)
+        if choice == 0:
+            # stop
+            return
+        try:
+            # try as function
+            action = prompts.get(choice)[1]
+            action()
+        except TypeError:
+            try:  # try as playground style
+                exec(open(action).read())
+            except FileNotFoundError:
+                print(f"File not found!: {action}")
+            # end function try
+        # end prompts try
+    except ValueError:
+        # not a number error
+        print(f"Not a number: {choice}")
+    except UnboundLocalError:
+        # traps all other errors
+        print(f"Invalid choice: {choice}")
+    # end validation try
+
+    buildMenu(banner, options)  # recursion, start menu over again
+
 
 if __name__ == "__main__":
-    presentMenu(mainMenu)
+    menu()
