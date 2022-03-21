0905c13466a4f25d1467d444229f56aa291cabde
nicm2 <89167131+nicm2@users.noreply.github.com>
2022-03-21 10:09:20 -0700

    Update fibonacci.py

--- week1/fibonacci.py
+++ week1/fibonacci.py
@@ -1,1 +1,15 @@
+def fibo(n):
+    if n == 1 or n==0:
+        return n 
+    return fibo(n-1) + fibo(n-2)
+
+def printFibo():
+    n = int(input("Enter a number: "))
+    output = [fibo(x+1) for x in range(n)]
+    print(*output)
+if __name__ == "__main__":
+    printFibo()
+© 2022 GitHub, Inc.
+Terms
+Privacy
 
